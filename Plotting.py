7fbe1e6fd53f4c6a4209fa16022fbd52157e5c28
olgeet <57223532+olgeet@users.noreply.github.com>
2021-02-17 15:09:13 +0100

    Create Plotting.py

---
+++ Plotting.py
@@ -0,0 +1,21 @@
+
+def PPlot(fig, x, y, Er_y=None, x_label="", y_label="", legName="", setLogY=False): #setting a default value of the input noFIt to False
+    
+    myPlot = fig.add_subplot()
+    ## first we do a the basic plot
+    if  Er_y is None: 
+        myPlot.plot(x, y, "o-", label=legName) ##makes plot without errorbars
+
+    elif Er_y is not None: ## errorsbars on Y but not on X
+        myPlot.errorbar(x, y, yerr=Er_y, fmt="o", label=legName, markersize=1, capsize=2., markerfacecolor="black", markeredgecolor="black") ##makes errorbar point plot
+
+    myPlot.set_xlabel(x_label, fontsize=16)
+    myPlot.set_ylabel(y_label, fontsize=16, labelpad = 10)
+    myPlot.grid(True)
+    
+    if setLogY:
+        myPlot.set_yscale('log') #### Logarithmic
+
+    myPlot.legend(fontsize=13)
+    
+    return myPlot
